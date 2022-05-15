34abb39453ccb3c8d420e6fb47744797672e4ec2
toleualtynbek <toleu.altinbek@mail.ru>
2022-05-16 00:42:51 +0600

    Task 1 finished

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,1 +1,11 @@
-print('Hello world')
+
+name = input("Введите ваше имя: ")
+age = int(input("Введите ваш возраст: "))
+
+s = "Привет"
+i = 5;
+
+print(s, name)
+print("Через ", i," лет ваш возраст будет", i + age)
+
+print(type(name))
